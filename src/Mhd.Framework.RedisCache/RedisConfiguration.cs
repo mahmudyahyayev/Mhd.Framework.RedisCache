383881103c5d9670f34cca2e5b07663c0a6d6b81
myahyayev <mahmud.yahyayev@sensormatic.com.tr>
2022-03-16 14:34:14 +0300

    InitialCreate

---
+++ src/Mhd.Framework.RedisCache/RedisConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Mhd.Framework.Ioc;
+
+namespace Mhd.Framewrok.RedisCache
+{
+    public class RedisConfiguration : IConfigurationOptions
+    {
+        public string Connection { get; set; }
+    }
+}
